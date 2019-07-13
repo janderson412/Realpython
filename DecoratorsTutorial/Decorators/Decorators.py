93783e1a656ac05316163b83f8dd394c1114a68d
Anderson, John [CLE] <john.e.anderson@medtronic.com>
2019-07-13 14:19:43 -0400

    Update decorators project.

--- DecoratorsTutorial/Decorators/Decorators.py
+++ DecoratorsTutorial/Decorators/Decorators.py
@@ -1,8 +1,11 @@
+import functools
+import time
 
 def do_twice(func):
+    @functools.wraps(func)
     def wrapper_do_twice(*args, **kwargs):
         func(*args, **kwargs)
-        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper_do_twice
 
 def do_twice_with_return(func):
@@ -11,4 +14,54 @@ def do_twice_with_return(func):
         return func(*args, **kwargs)
     return wrapper_do_twice_with_return
 
+
+def decorator(func):
+    @functools.wraps(func)
+    def wrapper_decorator(*args, **kwargs):
+        # Do something before running function
+        value = func(*args, **kwargs)
+        # Do something afterward
+        return value
+    return wrapper_decorator
+
+def timer(func):
+    '''Print the runtime of the decorated function'''
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        # Do something before running function
+        start_time = time.perf_counter()
+        value = func(*args, **kwargs)
+        # Do something afterward
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
+        print(f'Finished {func.__name__!r} in {run_time:.4f} seconds')
+        return value
+    return wrapper_timer
+
+def debug(func):
+    '''Print the function and arguments'''
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        # Do something before running function
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f'{k}={v!r}' for k,v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+        print(f'Calling {func.__name__}({signature})')
+        value = func(*args, **kwargs)
+        # Do something afterward
+        print(f'{func.__name__!r} returned {value!r}')
+        return value
+    return wrapper_debug
+
+
+def slow_down(func):
+    '''Sleep one second'''
+    @functools.wraps(func)
+    def wrapper_slowdown(*args, **kwargs):
+        # Do something before running function
+        time.sleep(1)
+        value = func(*args, **kwargs)
+        # Do something afterward
+        return value
+    return wrapper_slowdown
 
---
+++ DecoratorsTutorial/examples.py
@@ -0,0 +1,33 @@
+from Decorators.Decorators import *
+import math
+
+@do_twice
+def say_whee():
+    print("Whee!")
+
+
+@timer
+def waste_some_time(num_times):
+    for _ in range(num_times):
+        sum([i**2 for i in range(10000)])
+
+
+@debug
+def make_greeting(name, age=None):
+    if age is None:
+        return f'Howdy {name}!'
+    else:
+        return f'Howdy {name}, you are {age}'
+
+math.factorial = debug(math.factorial)
+
+def approximate_e(terms=18):
+    return sum(1 / math.factorial(n) for n in range(terms))
+
+@slow_down
+def countdown(from_number):
+    if from_number < 1:
+        print('Liftoff!!!')
+    else:
+        print(from_number)
+        countdown(from_number - 1)

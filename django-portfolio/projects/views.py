37730307d323cb8a913225f238d3cd923f89f192
Anderson, John [CLE] <john.e.anderson@medtronic.com>
2019-10-11 14:03:57 -0400

    Add model and DB to Django tutorial app.

--- django-portfolio/projects/views.py
+++ django-portfolio/projects/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from projects.models import Project
 from django.http import HttpResponse
 
 # Create your views here.
 
-def project_list(request):
-    return HttpResponse('<h1>Hello everyone!</h1>')
+#def project_list(request):
+    #return render(request, 'projects/index.html')
+
+def all_projects(request):
+    # query DB to return all project objects
+    projects = Project.objects.all()
+    return render(request, 'projects/all_projects.html', {'projects': projects})
 
